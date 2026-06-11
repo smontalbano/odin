5861ac0086c066cca4d128c2a7e5f7fd39ef7d6d
smontalbano <sdm030@gmail.com>
2026-06-10 23:48:32 -0400

    Add logic for caeser cipher

--- fs_ruby_on_rails/js_projects/caeser-cipher/caeserCipher.js
+++ fs_ruby_on_rails/js_projects/caeser-cipher/caeserCipher.js
@@ -1,5 +1,24 @@
 const alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
+function shift(word, key) {
+    let result = "";
+
+    for(const char of word) {
+        const index = alphaNumeric.indexOf(char);
+        
+        if(index === -1) {
+            result += char;
+            continue;
+        };
+
+        const newIndex = (index + key + alphaNumeric.length) % alphaNumeric.length;
+
+        result += alphaNumeric[newIndex];
+    };
+
+    return result;
+};
+
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -11,9 +30,9 @@ rl.question("Enter word: ", (word) => {
     rl.question("Enter key (must be an integer): ", (key) => {
         key = Number(key);
 
-        console.log(word)
-        console.log(key)
+        const encrypted = shift(word, key)
 
+        console.log(encrypted)
         rl.close();
     });
 });
