eda9b7e635034ac866be61b29776cd55256c30d6
smontalbano <sdm030@gmail.com>
2026-06-10 23:07:18 -0400

    refactoring

--- fs_ruby_on_rails/python/caesercipher/caeser_cipher.py
+++ fs_ruby_on_rails/python/caesercipher/caeser_cipher.py
@@ -3,21 +3,15 @@ def shift(key, word):
     result = ""
 
     for char in word:
-        for index, char2 in enumerate(alpha_numeric):
-            if char == char2:
-                new_index = (index + key) % len(alpha_numeric)
-                result = result + alpha_numeric[new_index]
-                break             
+        i = alpha_numeric.index(char)
+        new_i = (i + key) % len(alpha_numeric)
+        result += alpha_numeric[new_i]        
     return result
 
 alpha_numeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 word = input("Enter word: ")
 key = int(input("Enter key (must be an integer): "))
-
-
-
 result = shift(key, word)
 
 print(result)
-
